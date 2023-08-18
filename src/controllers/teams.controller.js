e001c4e786fd48c7ec77e264b63b4b75a265ed17
Joel7Anthony <anto777molina777@gmail.com>
2023-08-18 18:15:07 -0500

    team

--- src/controllers/teams.controller.js
+++ src/controllers/teams.controller.js
@@ -1,6 +1,17 @@
 const pool = require("../config/database");
-const team = require("../models/team.model");
+const teams = require("../models/team.model");
 
-TimeRanges.getListTeams = async(req, res) => {
-    
+Teams.getListTeams = async(req, res) => {
+   const teams = await pool.query('SELECT * FROM teams');
+   res.render('pages/teams/list-teams',{teams}) 
+};
+
+Teams.postTeams = async (req, res) => {
+    const {
+        name, photo, category, series, initials, description, mail, creation_date, main_color, secondary_color,phone
+    }
+    = req.body;
+    await pool.query("INSERT INTO teams set?", [newTeam]);
+    req.flash('success', 'creado');
+    res.redirect("/teams")
 }
--- src/routes/team.routes.js
+++ src/routes/team.routes.js
@@ -6,7 +6,9 @@ const teamsController = require('../controllers/teams.controller');
 //viws teams
 router.get('/', teamsController.getListTeams);
 router.post('/team', teamsController.postTeams);
-
-
+router.get('/list-teams', teamsController.getListTeams);
+router.get('delete-teams/:id', teamsController.deleteTeams);
+router.get('/edit-teams/:id', teamsController.getTeams);
+router.post('/edit-teams/:id',teamsController.updateUser);
 
 module.exports = router;
