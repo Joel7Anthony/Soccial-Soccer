a8f2333ebe2d1cae162d3afd73b93484bfbca91e
Joel7Anthony <anto777molina777@gmail.com>
2023-08-18 20:04:57 -0500

    orm y relacion completa

--- src/config/dataBase.orm.js
+++ src/config/dataBase.orm.js
@@ -18,6 +18,10 @@ const usersModel = require('../models/user.model');
 const playersModel = require('../models/player.model');
 const teamsModel = require('../models/team.model');
 const qualificationOctavosModel = require('../models/qualificationOctovo.model');
+const leagueRegularsModel = require('../models/leagueRegular.model');
+const qualificationFourthsModel = require('../models/qualificationFourth.model');
+const semifinalesModel = require('../models/semifinal.model');
+const finalesModel = require('../models/final.model');
 
 const sequelize = new Sequelize(
   'social_soccer_public',
@@ -53,17 +57,62 @@ sequelize.sync({ force: false })
 const users = usersModel(sequelize, Sequelize);
 const teams = teamsModel(sequelize, Sequelize);
 const players = playersModel(sequelize, Sequelize);
+const leagueRegulars = leagueRegularsModel(sequelize, Sequelize);
 const qualificationOctavos = qualificationOctavosModel(sequelize, Sequelize);
+const qualificationFourths = qualificationFourthsModel(sequelize, Sequelize);
+const semifinales = semifinalesModel(sequelize, Sequelize);
+const finales = finalesModel(sequelize, Sequelize);
 
 
 //Relaciones 
 
+users.hasMany(players);
+players.belongsTo(users);
 
+users.hasMany(teams);
+teams.belongsTo(users);
+
+users.hasMany(leagueRegulars);
+leagueRegulars.belongsTo(users);
+
+users.hasMany(qualificationOctavos);
+qualificationOctavos.belongsTo(users);
+
+users.hasMany(qualificationFourths);
+qualificationFourths.belongsTo(users);
+
+users.hasMany(semifinales);
+semifinales.belongsTo(users);
+
+users.hasMany(finales);
+finales.belongsTo(users);
+
+teams.hasMany(leagueRegulars);
+leagueRegulars.belongsTo(teams);
+
+teams.hasMany(players);
+players.belongsTo(teams);
+
+teams.hasMany(qualificationOctavos);
+qualificationOctavos.belongsTo(teams);
+
+teams.hasMany(qualificationFourths);
+qualificationFourths.belongsTo(teams);
+
+teams.hasMany(semifinales);
+semifinales.belongsTo(teams);
+
+teams.hasMany(finales);
+finales.belongsTo(teams);
 
 module.exports = {
   users,
   teams,
   players, 
-  qualificationOctavos  //Exportamos los modelos para usarlo en otros archivos
+  qualificationOctavos,
+  leagueRegulars,
+  qualificationFourths,
+  semifinales, 
+  finales //Exportamos los modelos para usarlo en otros archivos
 
 }
--- src/controllers/teams.controller.js
+++ src/controllers/teams.controller.js
@@ -2,35 +2,42 @@ const pool = require("../config/database");
 const teams = require("../models/team.model");
 const Teams = {};
 
-Teams.getListTeams = async(req, res) => {
-   const teams = await pool.query('SELECT * FROM teams');
-   res.render('pages/teams/list-teams',{teams}) 
+Teams.getListTeams = async (req, res) => {
+    const teams = await pool.query('SELECT * FROM teams');
+    res.render('pages/teams/list-teams', { teams })
 };
 
 Teams.postTeams = async (req, res) => {
     const {
-        name, photo, category, series, initials, description, mail, creation_date, main_color, secondary_color,phone
+        name, photo, category, series, initials, description, mail, creation_date, main_color, secondary_color, phone
     }
-    = req.body;
-    const newTeam = {name, photo, category, series, initials, description, mail, creation_date, main_color, secondary_color, phone}
+        = req.body;
+    const newTeam = { name, photo, category, series, initials, description, mail, creation_date, main_color, secondary_color, phone }
     await pool.query("INSERT INTO teams set?", [newTeam]);
     req.flash('success', 'creado');
     res.redirect("/teams/list-teams");
 };
 
 Teams.deleteTeams = async (req, res) => {
-    const { id} = req.params;
+    const { id } = req.params;
     await pool.query("DELETE FROM teams WHERE ID = ?", [id]);
 };
 
-Teams.getTeams = async ( req, res) => {
-    const {id} = req.params;
+Teams.getTeams = async (req, res) => {
+    const { id } = req.params;
     const team = await pool.query('SELECT * FROM teams WHERE id = ?', [id]);
     req.flash('success', 'bien');
-    res.render('pages/teams/edit-teams', {team: team[0]});
+    res.render('pages/teams/edit-teams', { team: team[0] });
 };
-Teams.updateTeam = async ( req, res) => {
-    const {id} = req.params;
+Teams.updateTeam = async (req, res) => {
+    const { id } = req.params;
+    const { name, photo, category, series, initials, description, mail, creation_date, main_color, secondary_color, phone } = req.body;
+    const newTeam = {
+        name, photo, category, series, initials, description, mail, creation_date, main_color, secondary_color, phone
+    };
+    await pool.query("UPDATE teams set ? WHERE id =?", [newTeam, id]);
+    req.flash('success' , 'Actualizado');
+    res.redirect('/teams/list-teams')
 }
 
 
