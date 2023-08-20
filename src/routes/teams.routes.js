8a9e1f716f65d0be3a559158b22f310a4ee44b0f
Joel7Anthony <anto777molina777@gmail.com>
2023-08-20 13:34:01 -0500

    router

--- src/routes/teams.routes.js
+++ src/routes/teams.routes.js
@@ -5,11 +5,13 @@ const { isLoggedIn } = require('../lib/auth');
 
 
 router.get('/', isLoggedIn, teamsController.getListTeams);
-router.get('/add', isLoggedIn, teamsController.getAddTeams);
 router.post('/teams', isLoggedIn,teamsController.postTeam);
+router.get('/add', isLoggedIn, teamsController.getAddTeams);
 router.get('/list-teams', isLoggedIn,teamsController.getListTeams);
 router.get('/delete-teams', isLoggedIn,teamsController.deleteTeam);
 router.get('/edit-teams/:id', isLoggedIn,teamsController.getTeam);
-router.post('/update-teams/:id', isLoggedIn,teamsController.updateTeam);
+router.post('/edit-teams/:id', isLoggedIn,teamsController.updateTeam);
+//router.post('/update-teams/:id', isLoggedIn,teamsController.updateTeam);
+
 
 module.exports = router;
